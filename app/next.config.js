ff912a8f055f606316ccf3cfa0a4ee170bd6cc6b
May Tusek <tusek.may@gmail.com>
2023-01-26 03:13:32 -0800

    Fixed config.

--- app/next.config.js
+++ app/next.config.js
@@ -16,6 +16,9 @@ const nextConfig = {
   reactStrictMode: true,
   pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
   basePath: '/blog',
+  images: {
+    unoptimized: true
+  }
 }
 
 module.exports = withMDX(nextConfig)
