1a2a9a3ddbd66ebdb3d7436fe94cbd3d15e453e3
May Tusek <tusek.may@gmail.com>
2023-01-26 19:05:41 -0800

    Added much better blog styling.

--- app/pages/_document.jsx
+++ app/pages/_document.jsx
@@ -3,8 +3,10 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
-      <title>may blog</title>
+      <Head>
+        <title>may blog</title>
+        <link rel="shortcut icon" href="/blog/favicon.ico" />
+      </Head>
       <body>
         <Main />
         <NextScript />
