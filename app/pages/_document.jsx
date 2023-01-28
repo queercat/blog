fc096d75946cc7d2661ed331c454981836f1d118
May Tusek <tusek.may@gmail.com>
2023-01-28 08:55:09 -0800

    Renamed things.

--- app/pages/_document.jsx
+++ app/pages/_document.jsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link rel="shortcut icon" href="/blog/favicon.ico" />
+        <link rel="shortcut icon" href="/favicon.ico" />
       </Head>
       <body>
         <Main />
