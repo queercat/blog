9f09d8a019ad36669cb9137cdcbf20a65456f877
May Tusek <tusek.may@gmail.com>
2023-01-30 02:44:00 -0800

    Fixed some weird styling issues.

---
+++ app/pages/tools/ahk-rebind.jsx
@@ -0,0 +1,8 @@
+/* {"title": "ahk remapper", "description": "easily make key rebinds per application with autohotkey."} */
+
+export default function AutoHotkeyRebind() {
+    return (
+        <div className>
+        </div>
+    )    
+}
